ae302ffe9e8f80aed13ec7a0f091db82448e0d98
Liyu Shumet <simplyliyu@gmail.com>
2024-03-02 10:18:41 +0300

    edited

--- index.py
+++ index.py
@@ -3,7 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def ruth():
+def ethiopia():
     return render_template("loading.html")
+@app.route("/baltina")
+def baltina():
+    return render_template("baltina.html")
 if __name__ == "__main__":
     app.run()
